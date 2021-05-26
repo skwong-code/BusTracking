fba91c4e89bb70d84468e22991d57432eefddf79
Stephan Wong <80276544+skwong-code@users.noreply.github.com>
2021-05-26 07:44:14 -0700

    refactor

--- mapanimation.js
+++ mapanimation.js
@@ -1,7 +1,5 @@
-// code from: https://docs.mapbox.com/mapbox-gl-js/example/geojson-line/ 
 // add your own access token
 mapboxgl.accessToken = 'pk.eyJ1Ijoic2t3b25nODMiLCJhIjoiY2tvbHNqN2F6MHEyejJwbzdoMWxwZzFhNiJ9.jorHl8yiNeUzDcAFwa_uRw';
-
 // This is the map instance
 let map = new mapboxgl.Map({
   container: 'map',
@@ -9,7 +7,6 @@ let map = new mapboxgl.Map({
   center: [-71.08927333556, 42.350803759733076],
   zoom: 13,
 });
-
 map.on('load', function () {
   map.addSource('route', {
     'type': 'geojson',
@@ -21,22 +18,17 @@ map.on('load', function () {
         }
     }
   });
-
   map.addLayer({
     'id': 'route',
     'type': 'line',
     'source': 'route',
     'layout': {
-    
     },
-
   });
 });
-  
 
 // This array contains the coordinates for all bus stops of route #1
 async function addBus(){
-
     // get bus data
     const locations = await getBusLocations();
     console.log(new Date());
@@ -45,16 +37,14 @@ async function addBus(){
     for (let i=0; i<= mapMarkers.length - 1; i++ ) {
     mapMarkers[i].remove();
     }
-    
     mapMarkers = [];
-
     for (let i = 0; i <= buses - 1; i++){
     // locations.forEach(function(marker) {
             if(locations[i].attributes.direction_id == 1){
             // create a HTML element for each feature
             var el = document.createElement('div');
             el.className = 'inbound';
-    
+   
         // make a marker for each feature and add to the map
         var newmarker = new mapboxgl.Marker(el)
         .setLngLat([locations[i].attributes.longitude, locations[i].attributes.latitude]).addTo(map);
@@ -73,7 +63,6 @@ async function addBus(){
     };
     setTimeout(addBus, 15000);
 }
-
 // Request bus data from MBTA
 async function getBusLocations(){
     const url = 'https://api-v3.mbta.com/vehicles';
@@ -81,7 +70,6 @@ async function getBusLocations(){
     const json     = await response.json();
     return json.data;
 }
-
 let mapMarkers = [];
 // fire the function
 addBus();
